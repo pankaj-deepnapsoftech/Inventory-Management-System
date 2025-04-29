55283c6e95eecb718236eada32a15a7fd20aa2fe
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-04-29 18:58:35 +0530

    UI design update

---
+++ frontend/.history/src/App_20250429143144.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Layout from "./pages/Layout";
+import routes from "./routes/routes";
+import { useSelector } from "react-redux";
+import NotFound from "./pages/NotFound";
+
+const App: React.FC = () => {
+
+  const { allowedroutes, isSuper } = useSelector((state: any) => state.auth);
+
+  return (
+    <div className="">
+      <img className="h-full " src="https://img.freepik.com/free-vector/abstract-big-data-digital-technology-background-design_1017-22920.jpg?t=st=1745917186~exp=1745920786~hmac=98b9b64f532c35706794ff5eeae7f4ac8362f4f2c7906b30c5479dea7ada7082&w=740" alt="" />
+      <div className="backdrop-blur-2xl  h-full w-full"></div>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Layout />}>
+            {routes.map((route, ind) => {
+              const isAllowed = isSuper || allowedroutes.includes(route.path.replaceAll('/', ''));
+              if (route.isSublink) {
+                return (
+                  <Route key={ind} path={route.path} element={route.element}>
+                    {route.sublink &&
+                      route.sublink.map((sublink, index) => {
+                        return (
+                          <Route
+                            key={index}
+                            path={sublink.path}
+                            element={sublink.element}
+                          ></Route>
+                        );
+                      })}
+                  </Route>
+                );
+              } else {
+                return (
+                  <Route
+                    index={route.name === "Dashboard" ? true : false}
+                    key={ind}
+                    path={route.path}
+                    element={route.element}
+                  ></Route>
+                );
+              }
+            })}
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/.history/src/components/Container/Container_20250429150922.tsx
@@ -0,0 +1,16 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div
+      className="w-full m-0 px-4 py-4 rounded overflow-auto bg-gradient-to-br from-[#312d50] via-[#205370] to-[]" 
+      style={{ boxShadow: "0 0 20px 3px rgba(221, 230, 240, 0.15)" }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
